55ab1ac6405608a021433ea02ce9b4fb5c9eadee
Alan Taylor <alan@aspenmesh.io>
2020-04-22 16:13:20 -0600

    with useFormik hook and validation schema with yup

--- src/index.tsx
+++ src/index.tsx
@@ -2,45 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { useFormik } from "formik";
-
-interface Values {
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
-interface Errors {
-  firstName: string | null;
-  lastName: string | null;
-  email: string | null;
-}
-
-const validate = (values: Values) => {
-  const errors: Errors = {
-    firstName: null,
-    lastName: null,
-    email: null,
-  };
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  return errors;
-};
+import * as Yup from "yup";
 
 const SignupForm = () => {
   const formik = useFormik({
@@ -49,7 +11,15 @@ const SignupForm = () => {
       lastName: "",
       email: "",
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
